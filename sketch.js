ed279951b69993584e0c60bd1d93dcee6b7b5555
Rich <richnyman@gmail.com>
2022-02-22 17:07:19 -0500

    animated gif

--- sketch.js
+++ sketch.js
@@ -23,8 +23,8 @@ const sketch = ({ context }) => {
   renderer.setClearColor("#000", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 500);
+  camera.position.set(3, 3, -5);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -36,15 +36,45 @@ const sketch = ({ context }) => {
   // Setup a geometry
   const geometry = new THREE.SphereGeometry(1, 32, 16);
 
+  // create textures
+  const loader = new THREE.TextureLoader();
+  const earthTexture = loader.load("earth.jpg");
+  const moonTexture = loader.load("moon.jpg");
+
   // Setup a material
-  const material = new THREE.MeshBasicMaterial({
-    color: "red",
-    wireframe: true
+  const earthMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalnes: 0,
+    map: earthTexture
+  });
+
+  const moonGroup = new THREE.Group();
+  const moonMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalnes: 0,
+    map: moonTexture
   });
 
   // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
+  const mesh = new THREE.Mesh(geometry, earthMaterial);
   scene.add(mesh);
+
+  const moonMesh = new THREE.Mesh(geometry, moonMaterial);
+  moonMesh.position.set(2, 1, 0);
+  moonMesh.scale.setScalar(0.15);
+  moonGroup.add(moonMesh);
+
+  scene.add(moonGroup);
+
+  // lighting
+  const light = new THREE.PointLight('white', 2.5);
+  light.position.set(0, 2, 0);
+  scene.add(light);
+  
+  // helpers
+  scene.add(new THREE.PointLightHelper(light, 0.15));
+  scene.add(new THREE.GridHelper(5, 50));
+  scene.add(new THREE.AxesHelper(20));
 
   // draw each frame
   return {
@@ -57,6 +87,10 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
+      mesh.rotation.y = time * 0.15;
+      moonMesh.rotation.y = time * -0.1;
+      moonMesh.rotation.z = time * -0.1;
+      moonGroup.rotation.y = time * .5;
       controls.update();
       renderer.render(scene, camera);
     },
